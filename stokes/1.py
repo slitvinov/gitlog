66ed94b4f1907909029d071ac9b9819c65700016
Sergey Litvinov <slitvinov@gmail.com>
2024-09-11 17:53:33 +0200

    up

---
+++ stokes/1.py
@@ -0,0 +1,75 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy
+
+
+def u(i, j):
+    return 0
+
+
+def boundaryp(i):
+    return i <= 0 or i >= m
+
+
+def f(i, d):
+    return i
+
+
+FIELDS = {("u", 0): 0, ("p", None): 1}
+
+
+def add(f, c, i, d=None):
+    if not boundaryp(i):
+        if i not in ik:
+            ik[i] = len(ik)
+        data.append(c)
+        col.append(nf * ik[i] + FIELDS[f, d])
+        row.append(len(rhs) - 1)
+    else:
+        assert f == "u"
+        val = 0
+        rhs[-1] -= c * val
+
+
+plt.rcParams["image.cmap"] = "jet"
+nf = len(FIELDS)
+m = 4
+ik = {}
+data = []
+col = []
+row = []
+rhs = []
+for i in range(-1, m + 1):
+    if not boundaryp(i) and not boundaryp(i - 1):
+        rhs.append(0)
+        add("u", 1, i - 1, 0)
+        add("u", -2, i, 0)
+        add("u", 1, i + 1, 0)
+        add("p", -1, i - 1)
+        add("p", 1, i)
+        rhs[-1] += f(i, 0)
+    if not boundaryp(i) or not boundaryp(i + 1):
+        rhs.append(0)
+        add("u", -1, i, 0)
+        add("u", 1, i + 1, 0)
+rhs.append(0)
+add("p", 1, 1)
+
+A = scipy.sparse.csr_matrix((data, (row, col)), dtype=float)
+#sol = scipy.sparse.linalg.spsolve(A, rhs)
+sol, *rest = scipy.sparse.linalg.lsqr(A, rhs)
+print(A * sol - rhs)
+
+print("unknown:", nf * len(ik))
+print("equations:", len(rhs))
+
+field = np.empty((nf, m + 1))
+field.fill(None)
+for i, k in ik.items():
+    l = nf * k
+    field[:, i] = sol[l:l + nf]
+for name, f in zip(("u", "p"), field):
+    plt.plot(f, "o-")
+plt.tight_layout()
+plt.savefig("1.png")
--- stokes/stokes.py
+++ stokes/stokes.py
@@ -2,80 +2,97 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy
+import itertools
+
 
 def u(i, j):
     return 0
 
-def boundary(i, j):
-    cond0 = i <= 0 or j <= 0 or i >= m or j >= m
-    return u(i, j) if cond0 else None
+
+def boundaryp(i, j):
+    return i <= 0 or j <= 0 or i >= m or j >= m
+
 
 def f(i, j, d):
-    return d
+    return d / h
 
-FIELDS = {
-    ("u", 0) : 0,
-    ("u", 1) : 1,
-    ("p", None) : 2}
 
 def add(f, c, i, j, d=None):
-    val = boundary(i, j)
-    if val is None:
+    if not boundaryp(i, j):
         if (i, j) not in ik:
             ik[i, j] = len(ik)
         data.append(c)
         col.append(nf * ik[i, j] + FIELDS[f, d])
         row.append(len(rhs) - 1)
     else:
+        assert f == "u"
+        val = 0
+        # val = 1 / h if j == m and d == 0 else 0
         rhs[-1] -= c * val
 
 
 plt.rcParams["image.cmap"] = "jet"
+FIELDS = {("u", 0): 0, ("u", 1): 1, ("p", None): 2}
 nf = len(FIELDS)
-m = 100
+m = 80
 ik = {}
 data = []
 col = []
 row = []
 rhs = []
+h = 1 / m
 for i in range(-1, m + 1):
     for j in range(-1, m + 1):
-        if boundary(i, j) is None:
+        if not boundaryp(i - 1, j) and not boundaryp(i, j):
             rhs.append(0)
-            add("u", 1, i-1, j, 0)
-            add("u", 1, i, j-1, 0)
+            add("u", 1, i - 1, j, 0)
+            add("u", 1, i, j - 1, 0)
             add("u", -4, i, j, 0)
-            add("u", 1, i, j+1, 0)
-            add("u", 1, i+1, j, 0)
-            add("p", -1, i-1, j)
+            add("u", 1, i, j + 1, 0)
+            add("u", 1, i + 1, j, 0)
+            add("p", -1, i - 1, j)
             add("p", 1, i, j)
-            rhs[-1] += f(i,j,0)
+            rhs[-1] += f(i, j, 0)
+        if not boundaryp(i, j - 1) and not boundaryp(i, j):
             rhs.append(0)
-            add("u", 1, i-1, j, 1)
-            add("u", 1, i, j-1, 1)
+            add("u", 1, i - 1, j, 1)
+            add("u", 1, i, j - 1, 1)
             add("u", -4, i, j, 1)
-            add("u", 1, i, j+1, 1)
-            add("u", 1, i+1, j, 1)
-            add("p", -1, i, j-1)
+            add("u", 1, i, j + 1, 1)
+            add("u", 1, i + 1, j, 1)
+            add("p", -1, i, j - 1)
             add("p", 1, i, j)
-            rhs[-1] += f(i,j,1)
+            rhs[-1] += f(i, j, 1)
+        if not boundaryp(i, j) or not boundaryp(i, j + 1) or not boundaryp(
+                i + 1, j):
             rhs.append(0)
             add("u", -1, i, j, 0)
             add("u", -1, i, j, 1)
-            add("u", 1, i, j+1, 1)
-            add("u", 1, i+1, j, 0)
+            add("u", 1, i, j + 1, 1)
+            add("u", 1, i + 1, j, 0)
+for i, j in itertools.product(range(-1, m + 1), range(-1, m + 1)):
+    if not boundaryp(i, j):
+        rhs.append(0)
+        add("p", 0, i, j)
+        break
 
+print("unknown:", nf * len(ik))
+print("equations:", len(rhs))
 A = scipy.sparse.csr_matrix((data, (row, col)), dtype=float)
-sol = scipy.sparse.linalg.spsolve(A, rhs)
+# sol = scipy.sparse.linalg.spsolve(A, rhs)
+sol, istop, itn, r1norm, r2norm, acond, *rest = scipy.sparse.linalg.lsqr(
+    A, rhs)
+print(istop, itn, r1norm, acond)
 
 field = np.empty((nf, m + 1, m + 1))
 field.fill(None)
 for (i, j), k in ik.items():
     l = nf * k
-    field[:, i, j] = sol[l : l + nf]
-
-for name, f in zip(("ux", "uy", "p"), field):
-    plt.imshow(f, origin="lower")
+    field[:, i, j] = sol[l:l + nf]
+for name, scale, f in zip(("ux", "uy", "p"), (h, h, h**2), field):
+    plt.imshow(f.T * scale, origin="lower")
+    plt.colorbar()
     plt.axis("off")
     plt.tight_layout()
     plt.savefig("stokes.%s.png" % name)
+    plt.close()
