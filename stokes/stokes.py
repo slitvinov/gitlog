943c91c903b6b6bb4d2c23a8a36a1da821721dec
Sergey Litvinov <slitvinov@gmail.com>
2024-09-12 12:04:48 +0200

    m = 150

--- stokes/stokes.py
+++ stokes/stokes.py
@@ -6,34 +6,40 @@ import itertools
 
 
 def boundaryp(i, j):
-    return i <= 0 or j <= 0 or i >= m or j >= m
+    return i <= 0 or j <= 0 or i >= m or j >= m or i >= j
 
 
-def f(i, j, d):
+def fu(i, j):
     return 0
 
 
-def add(c, f, i, j):
-    if not boundaryp(i, j):
-        if (f, i, j) not in ik:
-            ik[f, i, j] = len(ik)
+def fv(i, j):
+    return 0  # - 1 / scales["f"]
+
+
+def add(c, *idx):
+    f, *ij = idx
+    if f == "sigma" or not boundaryp(*ij):
+        if idx not in ik:
+            ik[idx] = len(ik)
         data.append(c)
-        col.append(ik[f, i, j])
+        col.append(ik[idx])
         row.append(len(rhs) - 1)
     else:
-        assert f == "u" or f == "v"
+        i, j = ij
         val = 1 / h if f == "u" and j == m else 0
         rhs[-1] -= c * val
 
 
 plt.rcParams["image.cmap"] = "jet"
-m = 50
+m = 150
 ik = {}
 data = []
 col = []
 row = []
 rhs = []
 h = 1 / m
+scales = {"u": h, "v": h, "p": 1, "f": 1 / h}
 for i, j in itertools.product(range(-1, m + 1), range(-1, m + 1)):
     if not boundaryp(i - 1, j) and not boundaryp(i, j):
         rhs.append(0)
@@ -44,7 +50,7 @@ for i, j in itertools.product(range(-1, m + 1), range(-1, m + 1)):
         add(1, "u", i + 1, j)
         add(-1, "p", i - 1, j)
         add(1, "p", i, j)
-        rhs[-1] += f(i, j, 0)
+        rhs[-1] += fu(i, j)
     if not boundaryp(i, j - 1) and not boundaryp(i, j):
         rhs.append(0)
         add(1, "v", i - 1, j)
@@ -54,10 +60,11 @@ for i, j in itertools.product(range(-1, m + 1), range(-1, m + 1)):
         add(1, "v", i + 1, j)
         add(-1, "p", i, j - 1)
         add(1, "p", i, j)
-        rhs[-1] += f(i, j, 1)
+        rhs[-1] += fv(i, j)
     if not boundaryp(i, j) or not boundaryp(i, j + 1) or not boundaryp(
             i + 1, j):
         rhs.append(0)
+        add(-h, "sigma")
         add(-1, "u", i, j)
         add(-1, "v", i, j)
         add(1, "v", i, j + 1)
@@ -67,21 +74,21 @@ for i, j in itertools.product(range(-1, m + 1), range(-1, m + 1)):
         rhs.append(0)
         add(1, "p", i, j)
         break
-# sol = scipy.sparse.linalg.spsolve(A, rhs)
 A = scipy.sparse.csr_matrix((data, (row, col)), dtype=float)
-sol, istop, itn, r1norm, r2norm, acond, *rest = scipy.sparse.linalg.lsqr(
-    A, rhs)
-print(f"{acond=} {r1norm=}")
+sol = scipy.sparse.linalg.spsolve(A, rhs)
 print("unknown:", len(ik))
 print("equations:", len(rhs))
-
-scales = {"u": h, "v": h, "p": 1}
+print("sigma:", sol[ik[
+    "sigma",
+]])
 names = "u", "v", "p"
 fields = {name: np.empty((m + 1, m + 1)) for name in names}
 for f in fields.values():
     f.fill(None)
-for (f, i, j), k in ik.items():
-    fields[f][i, j] = sol[k]
+for (name, *ij), k in ik.items():
+    if name in names:
+        i, j = ij
+        fields[name][i, j] = sol[k]
 for name, f in fields.items():
     plt.imshow(f.T * scales[name], origin="lower")
     plt.colorbar()
